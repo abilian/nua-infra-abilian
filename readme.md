cabb880852b0cfb0ca8d633d241f09c6b1f9a9a6
Stefane Fermigier <sf@fermigier.com>
2023-04-05 08:42:00 +0200

    feat: make it mode flexible.

--- readme.md
+++ readme.md
@@ -14,7 +14,7 @@ This assumes that the applications are in a subfolder `nua-apps` of the current 
 
 For instance, that there is a symblink from the `real-apps` folder of the `nua` sources to `./nua-apps`.
 
-You may also set the following env vars:
+You may also set the following env vars (or set a local `.env` file):
 
 - `NUA_DOMAIN`: domain (e.g.: apps.my-domain.com)
 - `NUA_HOST`: host where the orchestrator is running
--- tasks.py
+++ tasks.py
@@ -8,88 +8,120 @@ import toml as tomllib
 from pathlib import Path
 
 from invoke import task, Context
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+NUA_ENV = "/home/nua/env"
 
 DIM = "\033[2m"
 RESET = "\033[0m"
 
-apps = [
-    {"id": "galene", "name": "visio"},
-    {"id": "hedgedoc", "name": "pad"},
-    {"id": "ackee", "name": "stats"},
-    {"id": "dolibarr", "name": "dolibarr"},
-    {"id": "etherpad-lite", "name": "pad2"},
+# Default apps, but you can specify yours in the command-line
+# (eg. `invoke build --apps=galene,etherpad-lite`)
+APPS = [
+    {"name": "galene", "hostname": "visio"},
+    {"name": "hedgedoc", "hostname": "pad"},
+    {"name": "ackee", "hostname": "stats"},
+    {"name": "dolibarr", "hostname": "dolibarr"},
+    {"name": "etherpad-lite", "hostname": "pad2"},
 ]
+# APPS = []
 
 DEFAULT_DOMAIN = "c17.abilian.com"
 DEFAULT_HOST = "c17.abilian.com"
 DEFAULT_APPS_ROOT = f"{os.getcwd()}/nua-apps"
 
+# Read environment variables
 domain = os.environ.get("NUA_DOMAIN", DEFAULT_DOMAIN)
 host = os.environ.get("NUA_HOST", DEFAULT_HOST)
 apps_root = os.environ.get("NUA_APPS_ROOT", DEFAULT_APPS_ROOT)
 
 
 @task
-def all(c: Context):
+def all(c: Context, apps=None):
     """Build and deploy all apps."""
-    build(c)
-    deploy(c)
+    build(c, apps)
+    deploy(c, apps)
 
 
 @task
-def build(c: Context, _apps=None):
+def build(c: Context, apps=None):
     """Build apps. Use --apps=app1,app2,... to only build selected apps."""
-    if _apps in ("all", None):
-        for app in apps:
+    if apps in ("all", None):
+        for app in APPS:
             build_app(app)
     else:
-        app_ids = _apps.split(",")
+        app_ids = apps.split(",")
         for app_id in app_ids:
             app = get_app(app_id)
             build_app(app)
 
 
 @task
-def deploy(c: Context):
+def deploy(c: Context, apps=None):
     """Deploy all apps."""
     sites = []
-    for app in apps:
-        config_file = Path(f"{apps_root}/{app['id']}/nua-config.toml")
-        if not config_file.exists():
-            config_file = Path(f"{apps_root}/{app['id']}/nua/nua-config.toml")
-        config_data = config_file.read_text()
-        real_app_id = tomllib.loads(config_data)["metadata"]["id"]
-        app_deployment = {
-            "image": real_app_id,
-            "domain": f"{app['name']}.{domain}",
-        }
-        sites.append(app_deployment)
+    if apps in ("all", None):
+        for app in APPS:
+            sites.append(generate_deploy_config(app))
+    else:
+        app_ids = apps.split(",")
+        for app_id in app_ids:
+            app = get_app(app_id)
+            sites.append(generate_deploy_config(app))
 
     deployment = {"site": sites}
     Path("/tmp/nua-deployment.json").write_text(json.dumps(deployment, indent=2))
 
     if host == "localhost":
-        sh(f"/home/nua/nua310/bin/nua-orchestrator deploy /tmp/nua-deployment.json")
+        sh(f"{NUA_ENV}/bin/nua-orchestrator deploy /tmp/nua-deployment.json")
     else:
         sh(f"rsync -az /tmp/nua-deployment.json root@{host}:/tmp/nua-deployment.json")
-        ssh(f"/home/nua/nua310/bin/nua-orchestrator deploy /tmp/nua-deployment.json")
+        ssh(f"{NUA_ENV}/bin/nua-orchestrator deploy /tmp/nua-deployment.json")
 
 
-def build_app(app: dict[str, str]):
+def build_app(app: str | dict[str, str]):
     """Build a single app."""
-    app_id = app["id"]
+    app_name = app["name"]
     cwd = f"{apps_root}"
-    print(f"Building {app_id}...")
+    print(f"Building {app_name}...")
     if host == "localhost":
-        sh(f"/home/nua/nua310/bin/nua-build ./{app_id}", cwd=cwd)
+        sh(f"{NUA_ENV}/bin/nua-build ./{app_name}", cwd=cwd)
     else:
-        sh(f"rsync -e ssh -az ./{app_id} nua@{host}:/tmp/nua-apps/", cwd=cwd)
-        ssh(f"/home/nua/nua310/bin/nua-build /tmp/nua-apps/{app_id}")
+        sh(f"rsync -e ssh -az ./{app_name} nua@{host}:/tmp/nua-apps/", cwd=cwd)
+        ssh(f"{NUA_ENV}/bin/nua-build /tmp/nua-apps/{app_name}")
 
     # sh(f"nua-build .", cwd=cwd)
     print()
 
 
+def generate_deploy_config(app: dict[str, str]):
+    """Generate a nua-deployment.json file for a single app."""
+    app_name = app["name"]
+    config_data = get_config(app_name)
+    app_id = tomllib.loads(config_data)["metadata"]["id"]
+    app_hostname = app.get("hostname", app_id)
+    app_domain = f"{app_hostname}.{domain}"
+    app_deployment = {
+        "image": app_id,
+        "domain": app_domain,
+    }
+    return app_deployment
+
+
+def get_config(app_name):
+    config_file = Path(f"{apps_root}/{app_name}/nua-config.toml")
+    if not config_file.exists():
+        config_file = Path(f"{apps_root}/{app_name}/nua/nua-config.toml")
+    config_data = config_file.read_text()
+    return config_data
+
+
+#
+# Helpers
+#
 def sh(cmd: str, cwd: str = "."):
     """Run a shell command."""
     print(f"{DIM}Running {cmd} in {cwd}...{RESET}")
@@ -104,9 +136,10 @@ def ssh(cmd: str):
     subprocess.run(cmd, shell=True, check=True)
 
 
-def get_app(app_id: str) -> dict[str, str]:
+def get_app(app_name: str) -> dict[str, str]:
     """Get app info from app id."""
-    for app in apps:
-        if app["id"] == app_id:
+    for app in APPS:
+        if app["name"] == app_name:
             return app
-    raise ValueError(f"Unknown app {app_id}")
+    else:
+        return {"name": app_name}
